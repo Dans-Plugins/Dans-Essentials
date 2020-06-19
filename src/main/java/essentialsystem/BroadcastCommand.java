981ff9b5f88139c6555f71a574013d34ece7c040
dmccoystephenson <dmccoystephenson@gmail.com>
2020-06-18 18:02:23 -0600

    added permission to /broadcast command

--- src/main/java/essentialsystem/BroadcastCommand.java
+++ src/main/java/essentialsystem/BroadcastCommand.java
@@ -2,11 +2,19 @@ package essentialsystem;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class BroadcastCommand {
-    public void broadcast(String message) {
-        sendAllPlayersMessage(message);
+    public void broadcast(CommandSender sender, String message) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (!player.hasPermission("me.broadcast")) {
+                sender.sendMessage("Sorry! You need the 'me.broadcast' permission to use this command.");
+                return;
+            }
+            sendAllPlayersMessage(message);
+        }
     }
 
     public static void sendAllPlayersMessage(String message) {
