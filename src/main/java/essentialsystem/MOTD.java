148f325ff417f5098ccbd728f4b91257c1e46c89
dmccoystephenson <dmccoystephenson@gmail.com>
2020-06-19 11:10:46 -0600

    added load() method for MOTD.java

--- src/main/java/essentialsystem/MOTD.java
+++ src/main/java/essentialsystem/MOTD.java
@@ -1,8 +1,10 @@
 package essentialsystem;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class MOTD {
 
@@ -32,9 +34,9 @@ public class MOTD {
             }
             File saveFile = new File("./plugins/Medieval-Essentials/MessageOfTheDay.txt");
             if (saveFile.createNewFile()) {
-                System.out.println("Save file for message of the day created.");
+                System.out.println("Save file for the message of the day created.");
             } else {
-                System.out.println("Save file for message of the day already exists. Altering.");
+                System.out.println("Save file for the message of the day already exists. Altering.");
             }
 
             FileWriter saveWriter = new FileWriter("./plugins/Medieval-Essentials/" + "MessageOfTheDay.txt");
@@ -45,16 +47,30 @@ public class MOTD {
 
             saveWriter.close();
 
-            System.out.println("Successfully saved message of the day.");
+            System.out.println("Successfully saved the message of the day.");
 
         } catch (IOException e) {
-            System.out.println("An error occurred saving message of the day");
+            System.out.println("An error occurred saving the message of the day");
             e.printStackTrace();
         }
     }
 
     public void load() {
+        try {
+            File loadFile = new File("./plugins/Medieval-Essentials/MessageOfTheDay.txt");
+            Scanner loadReader = new Scanner(loadFile);
 
+            // actual loading
+            if (loadReader.hasNextLine()) {
+                setMessage(loadReader.nextLine());
+            }
+
+            loadReader.close();
+            System.out.println("Message of the day successfully loaded.");
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred loading the message of the day.");
+            e.printStackTrace();
+        }
     }
 
 }
