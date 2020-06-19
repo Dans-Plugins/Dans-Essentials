941688d8377aac236bb62a2528d6ea5b6f8218c6
dmccoystephenson <dmccoystephenson@gmail.com>
2020-06-19 11:07:54 -0600

    added save() method for MOTD.java

--- src/main/java/essentialsystem/MOTD.java
+++ src/main/java/essentialsystem/MOTD.java
@@ -1,5 +1,9 @@
 package essentialsystem;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class MOTD {
 
     private String message;
@@ -18,6 +22,39 @@ public class MOTD {
 
     public boolean isMessageSet() {
         return (message != null);
+    }
+
+    public void save() {
+        try {
+            File saveFolder = new File("./plugins/Medieval-Essentials/");
+            if (!saveFolder.exists()) {
+                saveFolder.mkdir();
+            }
+            File saveFile = new File("./plugins/Medieval-Essentials/MessageOfTheDay.txt");
+            if (saveFile.createNewFile()) {
+                System.out.println("Save file for message of the day created.");
+            } else {
+                System.out.println("Save file for message of the day already exists. Altering.");
+            }
+
+            FileWriter saveWriter = new FileWriter("./plugins/Medieval-Essentials/" + "MessageOfTheDay.txt");
+
+            // actual saving takes place here
+            saveWriter.write(message + "\n");
+
+
+            saveWriter.close();
+
+            System.out.println("Successfully saved message of the day.");
+
+        } catch (IOException e) {
+            System.out.println("An error occurred saving message of the day");
+            e.printStackTrace();
+        }
+    }
+
+    public void load() {
+
     }
 
 }
