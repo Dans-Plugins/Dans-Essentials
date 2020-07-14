872b68dd568f2e0d9d4038e4347af1cd98b6a11c
dmccoystephenson <dmccoystephenson@gmail.com>
2020-07-13 23:04:29 -0600

    added return statement

--- src/main/java/essentialsystem/Commands/FlySpeedCommand.java
+++ src/main/java/essentialsystem/Commands/FlySpeedCommand.java
@@ -7,36 +7,31 @@ import org.bukkit.entity.Player;
 public class FlySpeedCommand {
 
     public void setFlySpeed(CommandSender sender, String[] args) {
-        if (!(sender instanceof Player)) {
-            return;
-        }
+        if (sender instanceof Player) {
 
-        Player player = (Player) sender;
-
-        // if its the fly speed (number) command
-        if (args.length > 0 && args.length < 3) {
+            Player player = (Player) sender;
 
             if (player.hasPermission("medievalessentials.flyspeed") || player.hasPermission("medievalessentials.default")) {
                 // if not enough arguments
                 if (args.length == 0) {
                     player.sendMessage(ChatColor.RED + "Usage: /flyspeed (number)");
+                    return;
                 }
 
                 int speed = Integer.parseInt(args[0]);
 
                 if (player.getAllowFlight()) {
-                    player.setFlySpeed(speed); // TODO: fix null error here
-                }
-                else {
+                    player.setFlySpeed(speed);
+                } else {
                     player.sendMessage(ChatColor.RED + "You don't have flight enabled!");
                 }
 
-            }
-            else {
+            } else {
                 player.sendMessage(ChatColor.RED + "Sorry! In order to use this command, you need the following permission: 'medievalessentials.flyspeed");
             }
 
         }
+
     }
 
 }
