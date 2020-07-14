feb69fdbe32a5c8360e34fd51ab38b2b60d7dd10
dmccoystephenson <dmccoystephenson@gmail.com>
2020-07-13 23:19:41 -0600

    added messages

--- src/main/java/essentialsystem/Commands/FlySpeedCommand.java
+++ src/main/java/essentialsystem/Commands/FlySpeedCommand.java
@@ -18,11 +18,19 @@ public class FlySpeedCommand {
                     return;
                 }
 
-                int speed = Integer.parseInt(args[0]);
+                float speed = 0;
+                try {
+                    speed = (float) (Double.parseDouble(args[0])* 0.10);
+                } catch(Exception e) {
+                    player.sendMessage(ChatColor.RED + "Must be a number between 1-10");
+                    return;
+                }
 
-                if (speed > 0 && speed <= 10) {
+
+                if (speed > 0 && speed <= 1) {
                     if (player.getAllowFlight()) {
                         player.setFlySpeed(speed);
+                        player.sendMessage(ChatColor.GREEN + "Fly speed set to " + args[0]);
                     } else {
                         player.sendMessage(ChatColor.RED + "You don't have flight enabled!");
                     }
