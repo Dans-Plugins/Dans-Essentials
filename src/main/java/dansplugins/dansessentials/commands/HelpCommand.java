95a5ff9c8bad42163d4abad3d4e4fc99290edcf1
dmccoystephenson <dmccoystephenson@gmail.com>
2022-07-04 16:10:34 -0600

    Updated the help command.

--- src/main/java/dansplugins/dansessentials/commands/HelpCommand.java
+++ src/main/java/dansplugins/dansessentials/commands/HelpCommand.java
@@ -24,13 +24,14 @@ public class HelpCommand extends AbstractPluginCommand {
         commandSender.sendMessage(ChatColor.AQUA + "/de help - See a list of helpful commands.");
         commandSender.sendMessage(ChatColor.AQUA + "/de getpos - Get your coordinates.");
         commandSender.sendMessage(ChatColor.AQUA + "/de fly - Toggle flight for you or another player.");
-        commandSender.sendMessage(ChatColor.AQUA + "/de broadcast - Broadcast a message to everyone online.");
-        commandSender.sendMessage(ChatColor.AQUA + "/de mute - Mute a player until the next restart.");
-        commandSender.sendMessage(ChatColor.AQUA + "/de unmute - Unmute a player.");
+        commandSender.sendMessage(ChatColor.AQUA + "/de flyspeed <number> - Set your fly speed.");
+        commandSender.sendMessage(ChatColor.AQUA + "/de broadcast \"message\"- Broadcast a message to everyone online.");
+        commandSender.sendMessage(ChatColor.AQUA + "/de mute <ign> - Mute a player until the next restart.");
+        commandSender.sendMessage(ChatColor.AQUA + "/de unmute <ign> - Unmute a player.");
         commandSender.sendMessage(ChatColor.AQUA + "/de gm [ 0 | 1 | 2] - Set your gamemode.");
-        commandSender.sendMessage(ChatColor.AQUA + "/de label (name) - Rename an item.");
-        commandSender.sendMessage(ChatColor.AQUA + "/de invsee (player) - Interact with a player's inventory.");
-        commandSender.sendMessage(ChatColor.AQUA + "/de clearinv (player) - Clear a player's inventory.");
+        commandSender.sendMessage(ChatColor.AQUA + "/de label <name> - Rename an item.");
+        commandSender.sendMessage(ChatColor.AQUA + "/de invsee <player> - Interact with a player's inventory.");
+        commandSender.sendMessage(ChatColor.AQUA + "/de clearinv <player> - Clear a player's inventory.");
         return true;
     }
 
